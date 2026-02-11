4e3bac29664185267eec042a389eb3a9667c9f88
omar198017 <omar198017@yahoo.com>
2026-02-11 15:29:04 -0500

    1. Debugging - Python Arguments

---
+++ debugging/print_arguments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# Author: Omar Caguazango
+
+import sys
+
+# Loop through arguments starting from index 1 to skip the script name
+for argument in sys.argv[1:]:
+    print(argument)
+
